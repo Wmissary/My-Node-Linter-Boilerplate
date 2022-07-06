88c0cee5cd2f56bed627cde3eceb7939778cb109
Wmissary <pro.wmissary@gmail.com>
2022-07-06 17:53:29 +0200

    docs: add engine jsdoc

--- src/linter/engine.js
+++ src/linter/engine.js
@@ -1,5 +1,8 @@
 import Fs from "node:fs/promises";
 
+/** Add current node engine to package.json
+ * @param {Path} file - path to the package.json you want to modify
+ */
 const addNodeEngineToPackageJson = async (file) => {
   // eslint-disable-next-line security/detect-non-literal-fs-filename
   const packageData = JSON.parse(await Fs.readFile(file, "utf8"));
@@ -11,6 +14,9 @@ const addNodeEngineToPackageJson = async (file) => {
   await Fs.writeFile(file, JSON.stringify(packageData));
 };
 
+/** Remove current node engine from package.json
+ * @param {Path} file - path to the package.json you want to modify
+ */
 const removeNodeEngineFromPackageJson = async (file) => {
   //eslint-disable-next-line security/detect-non-literal-fs-filename
   const packageData = JSON.parse(await Fs.readFile(file, "utf8"));
